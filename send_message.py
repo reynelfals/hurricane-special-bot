82b27c4b580c9e5e7c05e6d5845beeaaafa4827d
Reynel Fals <reynelfals@gmail.com>
2021-07-17 00:03:52 -0400

    Iss #10 Add fix for message

--- send_message.py
+++ send_message.py
@@ -8,20 +8,14 @@ def sendMessage(chatid, text):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
     url_req = "https://api.telegram.org/bot" + config.API_KEY + "/sendMessage" + "?chat_id=" + chatid + "&text=" + text + "&parse_mode=HTML"
-    # url_req = "https://api.telegram.org/bot" + config.BOT_KEY + "/sendMessage" + "?chat_id=" + chatid + "&text=" + text
-    # url_req = "https://api.telegram.org/bot" + config.BOT_KEY + "/sendPhoto" + "?chat_id=" + chatid
 
     return requests.get(url_req, headers=headers)
-    # return requests.post(url=url_req, data={'photo':open("","rb")})
+
 
 def sendPhoto(chatid, filename, caption=None):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-    # url_req = "https://api.telegram.org/bot" + config.BOT_KEY + "/sendMessage" + "?chat_id=" + chatid + "&text=" + text + "&parse_mode=HTML"
-    # url_req = "https://api.telegram.org/bot" + config.BOT_KEY + "/sendMessage" + "?chat_id=" + chatid + "&text=" + text
     url_req = "https://api.telegram.org/bot" + config.API_KEY + "/sendPhoto"
-    # headers = {'content-type': 'application/x-www-form-urlencoded'}
-    # return requests.get(url_req, headers=headers)
     if caption is None:
         return requests.post(url=url_req, data={'chat_id': chatid}, files={'photo': open(filename, "rb")}, headers=headers)
     return requests.post(url=url_req, data={'chat_id':chatid, 'caption':caption}, files={'photo':open(filename,"rb")}, headers=headers)
